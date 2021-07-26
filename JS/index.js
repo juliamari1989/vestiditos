389f419b84ab54eb7f73260991d16bab8f6d1987
juliamari1989 <juliamari1989@gmail.com>
2021-07-26 00:27:10 -0300

    Update

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+
+var name = prompt('Ingrese su nombre');
+
+console.log('Hola', name);
+
+let usuario = prompt('Ingresá tu email o nombre de usuario:');
+
+alert('El email o nombre de usuario ingresado es: ' + usuario);
+
